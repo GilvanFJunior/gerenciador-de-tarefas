ea69a807cf2501dc3839c23d141cd9539363dfcc
junior_fran_11@hotmail.com <junior_fran_11@hotmail.com>
2024-12-19 16:55:36 -0300

    criação de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,35 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AddTask from './components/AddTask';
 import Tasks from './components/Tasks';
 import { v4 } from 'uuid';
+import Title from './components/Title';
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: 'Estudar Programação',
-      description:
-        'Estudar programação para poder se tornar um Programador Full Stack',
-      isCompleted: false,
-    },
-    {
-      id: 2,
-      title: 'Estudar Inglês',
-      description: 'Estudar para ser fluente em outra lingua',
-      isCompleted: false,
-    },
-    {
-      id: 3,
-      title: 'Estudar Galos de Combate',
-      description:
-        'Estudar sobre Galos de combate para poder ser o rei do  galismo',
-      isCompleted: false,
-    },
-  ]);
+  const [tasks, setTasks] = useState(
+    JSON.parse(localStorage.getItem('tasks')) || [],
+  );
+
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]);
+
+  useEffect(() => {
+    const fetchTasks = async () => {
+      // CHAMANDO A API
+      const response = await fetch(
+        'https://jsonplaceholder.typicode.com/todos?_limit=10',
+        {
+          method: 'GET',
+        },
+      );
+      //PEGAR OS DADOS QUE A API RETORNA
+      const data = await response.json();
+      setTasks(data);
+      //ARMAZENA OU PERSISTE OS DADOS NA STATE
+    };
+    //Se quiser chamar a API PARA PEGAR AS TAREFAS
+    //fetchTasks();
+  }, []);
 
   function onTaskClick(taskId) {
     const newTasks = tasks.map((task) => {
@@ -55,9 +59,7 @@ function App() {
   return (
     <div className="w-screen h-screen bg-slate-500 flex justify-center p-6">
       <div className="w-[500px] space-y-4">
-        <h1 className="text-3xl text-slate-100 font-bold text-center">
-          Gerenciador de Tarefas
-        </h1>
+        <Title>Gerenciador de Tarefas</Title>
         <AddTask onAddTaskSubmit={onAddTaskSubmit} />
         <Tasks
           tasks={tasks}
